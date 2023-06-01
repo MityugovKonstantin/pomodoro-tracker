a18c3f793392fd901ac1e3499d3c855575ed2a0d
Mityugov Konstantin <alxmira@yandex.ru>
2023-06-02 03:31:01 +0700

    Restart project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:pomodoro_tracker/screens/main.dart';
-import 'screens/timer.dart';
+import 'package:pomodoro_tracker/pages/main_page.dart';
+import 'package:pomodoro_tracker/pages/note_list_page.dart';
+import 'package:pomodoro_tracker/pages/pomodoro_page.dart';
+import 'package:pomodoro_tracker/pages/timer_page.dart';
 
-void main() {
-  runApp(MaterialApp(home: MainScreen()));
-}
+void main() => runApp(MaterialApp(
+  initialRoute: '/',
+  routes: {
+    '/': (context) => MainPage(),
+    '/timer': (context) => TimerPage(),
+    '/note_list': (context) => NoteListPage(),
+    '/pomodoro': (context) => PomodoroPage()
+  },
+));

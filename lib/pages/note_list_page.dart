232cd120125d85a2801775869fde6d80e710fb0b
Mityugov Konstantin <alxmira@yandex.ru>
2023-06-02 03:33:47 +0700

    Add feature pages

---
+++ lib/pages/note_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoteListPage extends StatefulWidget {
+  const NoteListPage({Key? key}) : super(key: key);
+
+  @override
+  State<NoteListPage> createState() => _NoteListPageState();
+}
+
+class _NoteListPageState extends State<NoteListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Записи');
+  }
+}

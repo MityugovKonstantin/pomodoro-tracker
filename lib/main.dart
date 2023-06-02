52063c0dc5dc91c2127743b862dd1d7e204bf90b
Mityugov Konstantin <alxmira@yandex.ru>
2023-06-03 03:56:37 +0700

    Add firebase db

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:pomodoro_tracker/pages/main_page.dart';
 import 'package:pomodoro_tracker/pages/note_list_page.dart';
+import 'package:pomodoro_tracker/pages/note_page.dart';
 import 'package:pomodoro_tracker/pages/pomodoro_page.dart';
 import 'package:pomodoro_tracker/pages/timer_page.dart';
 
-void main() => runApp(MaterialApp(
-  initialRoute: '/',
-  routes: {
-    '/': (context) => MainPage(),
-    '/timer': (context) => TimerPage(),
-    '/note_list': (context) => NoteListPage(),
-    '/pomodoro': (context) => PomodoroPage()
-  },
-));
+
+void main() {
+  initFirebase();
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => MainPage(),
+      '/timer': (context) => TimerPage(),
+      '/note_list': (context) => NoteListPage(),
+      '/note': (context) => NotePage(),
+      '/pomodoro': (context) => PomodoroPage()
+    },
+  ));
+}
+
+void initFirebase() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}

995e75733879cdfa769cd6d6af4ae9dbcbb229cf
Mityugov Konstantin <alxmira@yandex.ru>
2023-06-02 14:11:30 +0700

    Create timer
    A fully working timer has been created

--- lib/pages/timer_page.dart
+++ lib/pages/timer_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:pomodoro_tracker/utils/time_picker.dart';
 
 class TimerPage extends StatefulWidget {
   const TimerPage({Key? key}) : super(key: key);
@@ -8,8 +11,75 @@ class TimerPage extends StatefulWidget {
 }
 
 class _TimerPageState extends State<TimerPage> {
+
+  late Timer _timer;
+
+  bool _isTimerWork = false;
+  int _hour = 0;
+  int _minute = 0;
+  int _second = 0;
+
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if(_second > 0) {
+          _second--;
+        } else if (_minute > 0) {
+          _second = 59;
+          _minute--;
+        } else if (_hour > 0) {
+          _second = 59;
+          _minute = 59;
+          _hour--;
+        } else {
+          _isTimerWork = false;
+          _timer.cancel();
+        }
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Таймер'),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _isTimerWork
+                  ? Text(
+                  '$_hour:$_minute:$_second',
+                      style: const TextStyle(fontSize: 20))
+                  : TimePickerSpinner(
+                      isShowSeconds: true,
+                      onTimeChange: (time) {
+                        setState(() {
+                          _hour = time.hour;
+                          _minute = time.minute;
+                          _second = time.second;
+                        });
+                      }),
+              _isTimerWork ? const Text('')
+              : OutlinedButton(
+                  onPressed: () {
+                    setState(() {
+                      _isTimerWork = true;
+                    });
+                    _startTimer();
+                  },
+                  child: const Text('Старт'))
+            ],
+          ),
+        ));
   }
 }

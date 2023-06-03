b976093cd7b688098f814a51556e7beab7975e97
Mityugov Konstantin <alxmira@yandex.ru>
2023-06-04 02:38:30 +0700

    Remove unnecessary imports

--- lib/pages/note_page.dart
+++ lib/pages/note_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro_tracker/models/note.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 class NotePage extends StatefulWidget {
   const NotePage({Key? key}) : super(key: key);
